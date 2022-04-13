c118355a57ed6d276745a98bf3f68ae3269c215d
mohan338 <sahumohan338@gmail.com>
2022-04-13 11:32:36 +0530

    [mohan]add:basic logical

---
+++ BasicLogicProblem/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BasicLogicalProgramming;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine(" ********* Welcome in The Basic Logical Programming World ********");
+        Console.WriteLine("flip coin number 1\n Square of Two 2\n Leap Year 3\n Harmonic Number 4\n Prime Factor 5\n Quotient and Remainder 6\n Swapping 7\n EvenAnd Odd 8\n VowelAndConsonents 9\n");
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("Please Enter the valid Number To Execution of the Program ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin percentage = new FlipCoin();
+                    percentage.FlipCoins();
+                    break;
+                case 2:
+                    DisplayinSquare square = new DisplayinSquare();
+                    square.SquareOfTwo();
+                    break;
+                case 3:
+                    LeapYear year = new LeapYear();
+                    year.LeapYears();
+                    break;
+                case 4:
+                    HarmonicNumbers number = new HarmonicNumbers();
+                    number.HarmonicNumber();
+                    break;
+                case 5:
+                    PrimeFactor factor = new PrimeFactor();
+                    factor.PrimeFactors();
+                    break;
+                case 6:
+                    QuotientAndRemainder numbers = new QuotientAndRemainder();
+                    numbers.RemienderAndQuotient();
+                    break;
+                case 7:
+                    SwapTwoNumber swap = new SwapTwoNumber();
+                    swap.Swap();
+                    break;
+                case 8:
+                    EvenOrOdd checking = new EvenOrOdd();
+                    checking.Check();
+                    break;
+                case 9:
+                    VowelConsonents charector = new VowelConsonents();
+                    charector.VowelAndConsonent();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter the valid Number to Execution the Your Program");
+                    break;
+            }
+        }
+    }
+}
---
+++ BasicLogicProblem/VowelConsonents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgramming
+{
+    internal class VowelConsonents
+    {
+        public void VowelAndConsonent()
+        {
+            Console.WriteLine("Please Enter the word: ");
+            char ch = Console.ReadKey().KeyChar;
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                Console.WriteLine("Entered by End user Character is vowel in Lower case");
+            }
+            else if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("Entered by End user Character is vowel in Upper case");
+            }
+            else
+            {
+                Console.WriteLine("Entered by End user Character is Consonents");
+            }
+        }
+    }
+}
